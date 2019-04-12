a6bd3a94fdefd201336950aaa1e36bc4e32709a1
lulak21 <luiz@k21.com.br>
2019-04-12 12:12:06 -0300

    Correcao erro casa decimal

--- src/main/java/br/com/k21/CalculadoraComissao.java
+++ src/main/java/br/com/k21/CalculadoraComissao.java
@@ -3,7 +3,7 @@ package br.com.k21;
 public class CalculadoraComissao {
 
 	private static final int PERCENTUAL_FAIXA_SUPERIOR = 6;
-	private static final int PERCENTUAL_FAIXA_INFERIOR = 5;
+	private static final int PERCENTUAL_FAIXA_INFERIOR = 5; 
 
 	public static int calcular(int venda) {
 		if (venda <= 10000) {
@@ -15,7 +15,7 @@ public class CalculadoraComissao {
 
 	public static double calcular(double venda) {
 		if (venda <= 10000) {
-			return (venda * PERCENTUAL_FAIXA_INFERIOR / 100.00);
+			return Math.floor(100 * (venda * PERCENTUAL_FAIXA_INFERIOR / 100)) / 100;
 		} else {
 			return (venda * PERCENTUAL_FAIXA_SUPERIOR / 100.00);
 		}
